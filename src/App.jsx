fced91c90bc38ac23c1cc6713a62827e80f09dfd
moses. <88007849+moses0x@users.noreply.github.com>
2024-09-07 13:06:06 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import EarningsOverView from "./pages/EarningsOverview.jsx";
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
-      <EarningsOverView />
-=======
     <EarningsOverView />
->>>>>>> 0e105d3fdeda4e84c46e4f9d105754f990d435d0
     </>
   );
 };
